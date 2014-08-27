8c423d8bdecb0ec056e1863cc6d35b61a1b5b9ad
John Clements <clements@racket-lang.org>
2014-08-27 09:07:26 -0700

    added deps

--- info.rkt
+++ info.rkt
@@ -1,3 +1,12 @@
 #lang setup/infotab
 
 (define collection 'multi)
+
+(define deps
+  (list "base"
+        "rackunit-lib"
+        "typed-racket-lib"
+        "typed-racket-more"))
+
+(define build-deps
+  (list "scribble-lib"))
